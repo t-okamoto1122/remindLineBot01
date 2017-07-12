450964427c3f8880cbfa2d3ed1facf294298d512
“t-okamoto1122“ <taka.okamoto1122@gmail.com>
2017-07-11 20:31:05 -0500

    First commitcommit.

---
+++ src/main/java/com/example/demo/RemindLineBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindLineBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindLineBotApplication.class, args);
+	}
+}
